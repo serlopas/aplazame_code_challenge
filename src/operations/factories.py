8e7792610c77cb1b8954d22592d8312a6cf980cf
slopez <sergio.lopez@aplazame.com>
2022-03-20 00:49:20 +0100

    Split operations factories

--- src/operations/factories.py
+++ src/operations/factories.py
@@ -1,25 +1,27 @@
 import factory
-from factory import fuzzy
 
 from operations import models as operations_models
 from users import models as users_models
 
 
-class OperationFactory(factory.django.DjangoModelFactory):
+class OperationTopUpFactory(factory.django.DjangoModelFactory):
 
-    operation_type = fuzzy.FuzzyChoice(
-        [user_type for user_type, _ in operations_models.Operations.OPERATION_TYPES]
-    )
+    operation_type = operations_models.Operations.TOPUP
     amount = factory.Faker('pydecimal', right_digits=2, min_value=0, max_value=1000)
     wallet_from = factory.SubFactory(
         'wallets.factories.WalletFactory',
         user__user_type=users_models.User.CUSTOMER
     )
-    wallet_to = factory.SubFactory(
-        'wallets.factories.WalletFactory',
-        user__user_type=users_models.User.COMMERCE
-    )
     success = factory.Faker('pybool')
 
     class Meta:
         model = operations_models.Operations
+
+
+class OperationChargeFactory(OperationTopUpFactory):
+
+    operation_type = operations_models.Operations.CHARGE
+    wallet_to = factory.SubFactory(
+        'wallets.factories.WalletFactory',
+        user__user_type=users_models.User.COMMERCE
+    )
